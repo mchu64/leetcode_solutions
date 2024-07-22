226f231f7781f8c6891e06570c8f1d158504a63d
Michael Chu <mchu64@bu.edu>
2024-07-22 16:26:28 -0400

    Time: 116 ms (64.96%) | Memory: 12.2 MB (54.09%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.py
+++ 605-can-place-flowers/can-place-flowers.py
@@ -5,22 +5,18 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-        i = 0
-        while i < len(flowerbed):
+        for i in range(len(flowerbed)):
             if i == 0 and len(flowerbed)>1:
                 if flowerbed[0] == 0 and flowerbed[i+1] == 0:
                     flowerbed[0] = 1
-                    i+=1
                     n-=1
             if i > 0 and i < len(flowerbed)-1: 
                 if flowerbed[i-1] == 0 and flowerbed[i] == 0 and flowerbed[i+1] == 0:
                     n-=1
                     flowerbed[i] = 1
-                    i+=1
             if i == len(flowerbed)-1:
                 if flowerbed[i] == 0 and flowerbed[i-1] == 0:
                     flowerbed[i] = 1
                     n-=1
-            i+=1
         if n <= 0:
             return True
