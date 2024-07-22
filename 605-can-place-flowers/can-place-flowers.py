d51b6798b84cbb36b805e1198ebe0f6a7df76201
Michael Chu <mchu64@bu.edu>
2024-07-22 15:55:35 -0400

    Time: 123 ms (26.97%) | Memory: 11.9 MB (83.21%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.py
+++ 605-can-place-flowers/can-place-flowers.py
@@ -10,24 +10,22 @@ class Solution(object):
 
         while i < len(flowerbed):
             if i == 0 and len(flowerbed)>1:
-                after = i+1
-                if flowerbed[0] == 0 and flowerbed[after] == 0:
+                if flowerbed[0] == 0 and flowerbed[i+1] == 0:
                     flowerbed[0] = 1
                     i+=1
                     counter+=1
-            if i > 0 and i < len(flowerbed)-1:
-                last = i-1
-                current = i
-                after = i+1
-                if flowerbed[last] == 0 and flowerbed[current] == 0 and flowerbed[after] == 0:
+            if i > 0 and i < len(flowerbed)-1: 
+                if flowerbed[i-1] == 0 and flowerbed[i] == 0 and flowerbed[i+1] == 0:
                     counter += 1 
-                    flowerbed[current] = 1
+                    flowerbed[i] = 1
                     i+=1
 
             if i == len(flowerbed)-1:
                 if flowerbed[i] == 0 and flowerbed[i-1] == 0:
                     flowerbed[i] = 1
                     counter+=1
+
+
             i+=1
 
 
