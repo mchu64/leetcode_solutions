82722fa894696b9b6bc6ad236317a892d6a7fdcc
Michael Chu <mchu64@bu.edu>
2024-07-29 13:40:51 -0400

    Time: 36 ms (42.76%) | Memory: 15.1 MB (7.58%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -5,4 +5,11 @@ class Solution(object):
         :rtype: bool
         """
         s=''.join(char.lower() for char in s if char.isalnum())
-        return s == s[::-1]
+        left = len(s)-1
+
+        for x in range(len(s)-1):
+            if s[x] != s[left]:
+                return False
+            left=left-1
+        
+        return True
