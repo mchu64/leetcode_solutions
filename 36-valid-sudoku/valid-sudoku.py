0c473bcf6091f9fb48a1d90960c9f2be2b5ce9fd
Michael Chu <mchu64@bu.edu>
2024-07-31 10:01:21 -0400

    Time: 55 ms (92.46%) | Memory: 11.7 MB (26.78%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -4,9 +4,9 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: bool
         """
-        row = [set() for _ in range(9)]
-        col = [set() for _ in range(9)]
-        squares = [set() for _ in range(9)]
+        row = defaultdict(set)
+        col = defaultdict(set)
+        squares = defaultdict(set)
         
         for r in range(9):
             for c in range(9):
@@ -15,13 +15,11 @@ class Solution(object):
                 
                 value = board[r][c]
                 
-                # Check if value is in any of the sets
-                if value in row[r] or value in col[c] or value in squares[r // 3 * 3 + c // 3]:
+                if value in row[r] or value in col[c] or value in squares[(r // 3,c // 3)]:
                     return False
                 
-                # Add value to the appropriate sets
                 row[r].add(value)
                 col[c].add(value)
-                squares[r // 3 * 3 + c // 3].add(value)
+                squares[(r // 3,c // 3)].add(value)
 
         return True
