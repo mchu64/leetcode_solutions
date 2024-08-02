cd4afdcca40f6faca6f51aab819e792a04b5908e
Michael Chu <mchu64@bu.edu>
2024-08-02 15:13:00 -0400

    Time: 728 ms (87.56%) | Memory: 15 MB (52.15%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -4,31 +4,31 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        output = []
-        nums.sort()  # Sort the input list
 
-        for i in range(len(nums) - 2):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue  # Skip duplicates
-            
-            left = i + 1
-            right = len(nums) - 1
-            
+        answer = []
+        nums.sort()
+
+        for x in range(len(nums)-1):
+            if x > 0 and nums[x] == nums[x-1]:
+                continue
+            current=nums[x]
+            left = x+1
+            right = len(nums)-1
             while left < right:
-                total_sum = nums[i] + nums[left] + nums[right]
-                
-                if total_sum == 0:
-                    output.append([nums[i], nums[left], nums[right]])
-                    # Skip duplicates
-                    while left < right and nums[left] == nums[left + 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right - 1]:
-                        right -= 1
-                    left += 1
-                    right -= 1
-                elif total_sum < 0:
-                    left += 1
+                total = current + nums[left] + nums[right]
+                if total > 0:
+                    right-=1
+                elif total < 0:
+                    left+=1
                 else:
-                    right -= 1
+                    answer.append([current,nums[left],nums[right]])
+                    while left < right and nums[left] == nums[left+1]:
+                        left+=1
+                    while left < right and nums[right] == nums[right-1]:
+                        right-=1
+                    left+=1
+                    right-=1
+                    
         
-        return output
+        return answer
+
