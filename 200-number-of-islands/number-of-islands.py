43dfeeaa9b13d5c94cbcdb33ba8cb4e1eb1ed94e
Michael Chu <mchu64@bu.edu>
2025-10-21 12:33:06 -0400

    Time: 259 ms (27.85%) | Memory: 34.4 MB (5.58%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        #iterate through each piece of land on the grid grid[x][y] and run dfs on each keep track using a visited set
+
+        visited = set()
+        rows = len(grid)
+        cols = len(grid[0])
+        islands = 0
+
+        def dfs(row,col):
+            #make sure we are still in bounds:
+            if(row < 0 or col < 0 or row >= rows or col >= cols) or (row,col) in visited or grid[row][col] == '0':
+                    return
+            visited.add((row,col))
+            dfs(row+1,col)
+            dfs(row-1,col)
+            dfs(row,col+1)
+            dfs(row,col-1)
+            
+
+
+        for row in range(rows):
+            for col in range(cols):
+                if (row,col) not in visited and grid[row][col] == '1':
+                    islands += 1
+                    dfs(row,col)
+        return islands
+
